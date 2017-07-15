6a8b5b2153f6a585bbd78a6ccacc01df6ee44c76
XadillaX <admin@xcoder.in>
2017-07-15 12:38:53 +0800

    tls: deprecate parseCertString & move to internal
    `tls.parseCertString()` exposed by accident. Now move this function to
    `internal/tls` and mark the original one as deprecated.
    
    Refs: https://github.com/nodejs/node/issues/14193
    Refs: https://github.com/nodejs/node/commit/af80e7bc6e6f33c582eb1f7d37c7f5bbe9f910f7#diff-cc32376ce1eaf679ec2298cd483f15c7R188

--- test/parallel/test-tls-parse-cert-string.js
+++ test/parallel/test-tls-parse-cert-string.js
@@ -1,15 +1,23 @@
 'use strict';
+
+// Flags: --expose_internals
 const common = require('../common');
 if (!common.hasCrypto)
   common.skip('missing crypto');
 
 const assert = require('assert');
+const internalTLS = require('internal/tls');
 const tls = require('tls');
+
+const noOutput = common.mustNotCall();
+common.hijackStderr(function() {
+  process.nextTick(noOutput);
+});
 
 {
   const singles = 'C=US\nST=CA\nL=SF\nO=Node.js Foundation\nOU=Node.js\n' +
                   'CN=ca1\nemailAddress=ry@clouds.org';
-  const singlesOut = tls.parseCertString(singles);
+  const singlesOut = internalTLS.parseCertString(singles);
   assert.deepStrictEqual(singlesOut, {
     C: 'US',
     ST: 'CA',
@@ -24,7 +32,7 @@ const tls = require('tls');
 {
   const doubles = 'OU=Domain Control Validated\nOU=PositiveSSL Wildcard\n' +
                   'CN=*.nodejs.org';
-  const doublesOut = tls.parseCertString(doubles);
+  const doublesOut = internalTLS.parseCertString(doubles);
   assert.deepStrictEqual(doublesOut, {
     OU: [ 'Domain Control Validated', 'PositiveSSL Wildcard' ],
     CN: '*.nodejs.org'
@@ -33,6 +41,17 @@ const tls = require('tls');
 
 {
   const invalid = 'fhqwhgads';
-  const invalidOut = tls.parseCertString(invalid);
+  const invalidOut = internalTLS.parseCertString(invalid);
   assert.deepStrictEqual(invalidOut, {});
+}
+
+common.restoreStderr();
+
+{
+  common.expectWarning('DeprecationWarning',
+                       'tls.parseCertString() is deprecated. ' +
+                       'Please use querystring.parse() instead.');
+
+  const ret = tls.parseCertString('foo=bar');
+  assert.deepStrictEqual(ret, { foo: 'bar' });
 }
